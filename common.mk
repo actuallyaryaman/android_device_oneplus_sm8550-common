13f9d7aefd4dc3e8a52412b236d051c19a47ca9b
gaurav-paul9 <paulgaurav9@gmail.com>
2026-07-06 22:19:45 +0530

    Revert "DeviceSettings: Add bypass charging quick settings tile"
    This reverts commit 4dba8291813bfbd26e96275af1db5a0cdf1499db.

--- common.mk
+++ common.mk
@@ -454,7 +454,3 @@ PRODUCT_PACKAGES += \
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8550-common/sm8550-common-vendor.mk)
-
-#Bypass Charging
-PRODUCT_PACKAGES += \
-    DeviceSettings
